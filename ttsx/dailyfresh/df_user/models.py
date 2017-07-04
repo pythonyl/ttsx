519cc954d4727fb473e24ce97c13e5d445e1ab44
yaole <yaolepython@126.com>
2017-07-04 21:05:37 +0800

    配置和模型类

---
+++ ttsx/dailyfresh/df_user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20,db_column='name')
+    upwd = models.CharField(max_length=40,db_column='pwd')
+
+    def __str__(self):
+        return '%d' % self.pk
